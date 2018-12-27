6268bb4c04b5bf1527ee76b6b22347eb4d4d22fc
Jaap Rood <jaap@jaaprood.nl>
2018-12-28 08:47:11 +1000

    Setup Travis CI

--- __tests__/__helpers__.js
+++ __tests__/__helpers__.js
@@ -1,5 +1,5 @@
-process.env.DATABASE_URL = 'postgres://localhost:5432/klein_test';
-process.env.TEST_DATABASE_URL = 'postgres://localhost:5432/klein_test';
+process.env.DATABASE_URL = process.env.DATABASE_URL || 'postgres://localhost:5432/klein_test';
+process.env.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'postgres://localhost:5432/klein_test';
 
 const Knex = require('knex');
 const Schema = require('../schema').createSchema({ log: false });
